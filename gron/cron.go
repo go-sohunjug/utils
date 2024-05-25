b0865d0302be7ae49987d853202777862b587883
sohunjug <sohunjug@hotmail.com>
2024-05-26 00:43:42 +0800

    [update] gron

--- gron/cron.go
+++ gron/cron.go
@@ -1,229 +1,372 @@
 package gron
 
 import (
-	"fmt"
+	"context"
 	"sort"
+	"sync"
 	"time"
 
 	"github.com/go-sohunjug/utils/ants"
 	"github.com/google/uuid"
 )
 
-// Entry consists of a schedule and the job to be executed on that schedule.
-type Entry struct {
-	Schedule Schedule
-	Job      Job
-	Name     string
-
-	// the next time the job will run. This is zero time if Cron has not been
-	// started or invalid schedule.
-	Next time.Time
-
-	// the last time the job was run. This is zero time if the job has not been
-	// run.
-	Prev time.Time
+// Cron keeps track of any number of entries, invoking the associated func as
+// specified by the schedule. It may be started, stopped, and the entries may
+// be inspected while running.
+type Cron struct {
+	entries   []*Entry
+	chain     Chain
+	stop      chan struct{}
+	add       chan *Entry
+	remove    chan interface{}
+	snapshot  chan chan []Entry
+	running   bool
+	logger    Logger
+	runningMu sync.Mutex
+	location  *time.Location
+	parser    ScheduleParser
+	nextID    EntryID
+	jobWaiter sync.WaitGroup
 }
 
-// byTime is a handy wrapper to chronologically sort entries.
-type byTime []*Entry
-
-func (b byTime) Len() int      { return len(b) }
-func (b byTime) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
-
-// Less reports `earliest` time i should sort before j.
-// zero time is not `earliest` time.
-func (b byTime) Less(i, j int) bool {
-	if b[i].Next.IsZero() {
-		return false
-	}
-	if b[j].Next.IsZero() {
-		return true
-	}
-
-	return b[i].Next.Before(b[j].Next)
+// ScheduleParser is an interface for schedule spec parsers that return a Schedule
+type ScheduleParser interface {
+	Parse(spec string) (Schedule, error)
 }
 
-// Job is the interface that wraps the basic Run method.
-//
-// Run executes the underlying func.
+// Job is an interface for submitted cron jobs.
 type Job interface {
 	Run()
 }
 
-// Cron provides a convenient interface for scheduling job such as to clean-up
-// database entry every month.
+// Schedule describes a job's duty cycle.
+type Schedule interface {
+	// Next returns the next activation time, later than the given time.
+	// Next is invoked initially, and then each time the job is run.
+	Next(time.Time) time.Time
+}
+
+// EntryID identifies an entry within a Cron instance
+type EntryID int
+
+// Entry consists of a schedule and the func to execute on that schedule.
+type Entry struct {
+	// ID is the cron-assigned ID of this entry, which may be used to look up a
+	// snapshot or remove it.
+	ID EntryID
+
+	Name string
+
+	// Schedule on which this job should be run.
+	Schedule Schedule
+
+	// Next time the job will run, or the zero time if Cron has not been
+	// started or this entry's schedule is unsatisfiable
+	Next time.Time
+
+	// Prev is the last time this job was run, or the zero time if never.
+	Prev time.Time
+
+	// WrappedJob is the thing to run when the Schedule is activated.
+	WrappedJob Job
+
+	// Job is the thing that was submitted to cron.
+	// It is kept around so that user code that needs to get at the job later,
+	// e.g. via Entries() can do so.
+	Job Job
+}
+
+// Valid returns true if this is not the zero entry.
+func (e Entry) Valid() bool { return e.ID != 0 }
+
+// byTime is a wrapper for sorting the entry array by time
+// (with zero time at the end).
+type byTime []*Entry
+
+func (s byTime) Len() int      { return len(s) }
+func (s byTime) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s byTime) Less(i, j int) bool {
+	// Two zero times should return false.
+	// Otherwise, zero is "greater" than any other time.
+	// (To sort it at the end of the list.)
+	if s[i].Next.IsZero() {
+		return false
+	}
+	if s[j].Next.IsZero() {
+		return true
+	}
+	return s[i].Next.Before(s[j].Next)
+}
+
+// New returns a new Cron job runner, modified by the given options.
 //
-// Cron keeps track of any number of entries, invoking the associated func as
-// specified by the schedule. It may also be started, stopped and the entries
-// may be inspected.
-type Cron struct {
-	entries []*Entry
-	running bool
-	pool    ants.Pool
-	add     chan *Entry
-	stop    chan string
-}
-
-// New instantiates new Cron instant c.
-func New() *Cron {
-	return &Cron{
-		stop:    make(chan string),
-		add:     make(chan *Entry),
-		running: false,
-	}
-}
-
-// Start signals cron instant c to get up and running.
-func (c *Cron) Start() {
-	if !c.running {
-		c.running = true
-		ants.Submit(c.run)
-	}
-}
-
-// Add appends schedule, job to entries.
+// Available Settings
 //
-// if cron instant is not running, adding to entries is trivial.
-// otherwise, to prevent data-race, adds through channel.
-func (c *Cron) Add(s Schedule, j Job) string {
-  uuidV1, _ := uuid.NewUUID()
-	entry := &Entry{
-		Schedule: s,
-		Job:      j,
-    Name: uuidV1.String(),
+//	Time Zone
+//	  Description: The time zone in which schedules are interpreted
+//	  Default:     time.Local
+//
+//	Parser
+//	  Description: Parser converts cron spec strings into cron.Schedules.
+//	  Default:     Accepts this spec: https://en.wikipedia.org/wiki/Cron
+//
+//	Chain
+//	  Description: Wrap submitted jobs to customize behavior.
+//	  Default:     A chain that recovers panics and logs them to stderr.
+//
+// See "cron.With*" to modify the default behavior.
+func New(opts ...Option) *Cron {
+	c := &Cron{
+		entries:   nil,
+		chain:     NewChain(),
+		add:       make(chan *Entry),
+		stop:      make(chan struct{}),
+		snapshot:  make(chan chan []Entry),
+		remove:    make(chan interface{}),
+		running:   false,
+		runningMu: sync.Mutex{},
+		logger:    DefaultLogger,
+		location:  time.Local,
+		parser:    standardParser,
 	}
+	for _, opt := range opts {
+		opt(c)
+	}
+	return c
+}
 
+// FuncJob is a wrapper that turns a func() into a cron.Job
+type FuncJob func()
+
+func (f FuncJob) Run() { f() }
+
+// AddFunc adds a func to the Cron to be run on the given schedule.
+// The spec is parsed using the time zone of this Cron instance as the default.
+// An opaque ID is returned that can be used to later remove it.
+func (c *Cron) AddFunc(spec string, cmd func()) (EntryID, string, error) {
+	return c.AddJob(spec, FuncJob(cmd))
+}
+
+// AddJob adds a Job to the Cron to be run on the given schedule.
+// The spec is parsed using the time zone of this Cron instance as the default.
+// An opaque ID is returned that can be used to later remove it.
+func (c *Cron) AddJob(spec string, cmd Job) (EntryID, string, error) {
+	schedule, err := c.parser.Parse(spec)
+	if err != nil {
+		return 0, "", err
+	}
+	uuidV1, _ := uuid.NewUUID()
+	entry := c.CreateEntry(schedule, uuidV1.String(), cmd)
+	return entry.ID, entry.Name, nil
+}
+
+func (c *Cron) AddHandlerFunc(name, spec string, cmd func()) (EntryID, string, error) {
+	return c.AddJob(spec, FuncJob(cmd))
+}
+
+func (c *Cron) AddHandlerJob(name, spec string, cmd Job) (EntryID, string, error) {
+	schedule, err := c.parser.Parse(spec)
+	if err != nil {
+		return 0, "", err
+	}
+	entry := c.CreateEntry(schedule, name, cmd)
+	return entry.ID, entry.Name, nil
+}
+
+// Schedule adds a Job to the Cron to be run on the given schedule.
+// The job is wrapped with the configured Chain.
+func (c *Cron) CreateEntry(schedule Schedule, name string, cmd Job) *Entry {
+	c.runningMu.Lock()
+	defer c.runningMu.Unlock()
+  entry := c.Entry(name)
+  if c.Entry(name) != nil {
+    return entry
+  }
+	c.nextID++
+	entry = &Entry{
+		ID:         c.nextID,
+		Name:       name,
+		Schedule:   schedule,
+		WrappedJob: c.chain.Then(cmd),
+		Job:        cmd,
+	}
 	if !c.running {
 		c.entries = append(c.entries, entry)
-		return uuidV1.String()
+	} else {
+		c.add <- entry
 	}
-	c.add <- entry
-  return uuidV1.String()
+	return entry
 }
 
-// AddFunc registers the Job function for the given Schedule.
-func (c *Cron) AddFunc(s Schedule, j func()) string {
-	return c.Add(s, JobFunc(j))
-}
-
-// Add appends schedule, job to entries.
-//
-// if cron instant is not running, adding to entries is trivial.
-// otherwise, to prevent data-race, adds through channel.
-func (c *Cron) AddHanderJob(n string, s Schedule, j Job) (err error) {
-	for _, e := range c.entries {
-		if e.Name == n {
-			err = fmt.Errorf("hander %s already in scheduler", n)
-			return
-		}
-	}
-	entry := &Entry{
-		Schedule: s,
-		Job:      j,
-		Name:     n,
-	}
-
-	if !c.running {
-		c.entries = append(c.entries, entry)
-		return
-	}
-	c.add <- entry
-	return
-}
-
-// AddFunc registers the Job function for the given Schedule.
-func (c *Cron) AddHandlerFunc(n string, s Schedule, j func()) (err error) {
-	err = c.AddHanderJob(n, s, JobFunc(j))
-	return
-}
-
-func (c *Cron) RemoveHandler(n string) {
-	if !c.running {
-		return
-	}
-	c.stop <- n
-}
-
-// Stop halts cron instant c from running.
-func (c *Cron) Stop() {
-	if !c.running {
-		return
-	}
-	c.running = false
-	c.stop <- "_all_jobs_"
-}
-
-var after = time.After
-
-// run the scheduler...
-//
-// It needs to be private as it's responsible of synchronizing a critical
-// shared state: `running`.
-func (c *Cron) run() {
-	var effective time.Time
-	now := time.Now().Local()
-
-	// to figure next trig time for entries, referenced from now
-	for _, e := range c.entries {
-		e.Next = e.Schedule.Next(now)
-	}
-
-	for {
-		sort.Sort(byTime(c.entries))
-		if len(c.entries) > 0 {
-			effective = c.entries[0].Next
-		} else {
-			effective = now.AddDate(15, 0, 0) // to prevent phantom jobs.
-		}
-
-		select {
-		case now = <-after(effective.Sub(now)):
-			// entries with same time gets run.
-			for _, entry := range c.entries {
-				if entry.Next != effective {
-					break
-				}
-				entry.Prev = now
-				entry.Next = entry.Schedule.Next(now)
-				ants.Submit(entry.Job.Run)
-			}
-		case e := <-c.add:
-			e.Next = e.Schedule.Next(time.Now())
-			c.entries = append(c.entries, e)
-		case n := <-c.stop:
-			if n == "_all_jobs_" {
-				return // terminate go-routine.
-			}
-			for i, e := range c.entries {
-				if e.Name == n {
-					c.entries = append(c.entries[:i], c.entries[i+1:]...)
-					break
-				}
-			}
-		}
-	}
-}
-
-func (c *Cron) List() (list []string) {
-	for _, e := range c.entries {
-		list = append(list, e.Name)
-	}
-	return list
-}
-
-// Entries returns cron etn
+// Entries returns a snapshot of the cron entries.
 func (c *Cron) Entries() []*Entry {
 	return c.entries
 }
 
-// JobFunc is an adapter to allow the use of ordinary functions as gron.Job
-// If f is a function with the appropriate signature, JobFunc(f) is a handler
-// that calls f.
-//
-// todo: possibly func with params? maybe not needed.
-type JobFunc func()
+// Location gets the time zone location
+func (c *Cron) Location() *time.Location {
+	return c.location
+}
 
-// Run calls j()
-func (j JobFunc) Run() {
-	j()
+// Entry returns a snapshot of the given entry, or nil if it couldn't be found.
+func (c *Cron) Entry(id interface{}) *Entry {
+	for _, entry := range c.entries {
+		if value, ok := id.(EntryID); ok {
+			if value == entry.ID {
+				return entry
+			}
+		} else if value, ok := id.(string); ok {
+			if value == entry.Name {
+				return entry
+			}
+		}
+	}
+	return nil
+}
+
+// Remove an entry from being run in the future.
+func (c *Cron) Remove(id interface{}) {
+	c.runningMu.Lock()
+	defer c.runningMu.Unlock()
+	if c.running {
+		c.remove <- id
+	} else {
+		c.removeEntry(id)
+	}
+}
+
+// Start the cron scheduler in its own goroutine, or no-op if already started.
+func (c *Cron) Start() {
+	c.runningMu.Lock()
+	defer c.runningMu.Unlock()
+	if c.running {
+		return
+	}
+	c.running = true
+	ants.Submit(c.run)
+}
+
+// Run the cron scheduler, or no-op if already running.
+func (c *Cron) Run() {
+	c.runningMu.Lock()
+	if c.running {
+		c.runningMu.Unlock()
+		return
+	}
+	c.running = true
+	c.runningMu.Unlock()
+	c.run()
+}
+
+// run the scheduler.. this is private just due to the need to synchronize
+// access to the 'running' state variable.
+func (c *Cron) run() {
+	c.logger.Info("start")
+
+	// Figure out the next activation times for each entry.
+	now := c.now()
+	for _, entry := range c.entries {
+		entry.Next = entry.Schedule.Next(now)
+		c.logger.Info("schedule", "now", now, "entry", entry.ID, "next", entry.Next)
+	}
+
+	for {
+		// Determine the next entry to run.
+		sort.Sort(byTime(c.entries))
+
+		var timer *time.Timer
+		if len(c.entries) == 0 || c.entries[0].Next.IsZero() {
+			// If there are no entries yet, just sleep - it still handles new entries
+			// and stop requests.
+			timer = time.NewTimer(100000 * time.Hour)
+		} else {
+			timer = time.NewTimer(c.entries[0].Next.Sub(now))
+		}
+
+		for {
+			select {
+			case now = <-timer.C:
+				now = now.In(c.location)
+				c.logger.Info("wake", "now", now)
+
+				// Run every entry whose next time was less than now
+				for _, e := range c.entries {
+					if e.Next.After(now) || e.Next.IsZero() {
+						break
+					}
+					c.startJob(e.WrappedJob)
+					e.Prev = e.Next
+					e.Next = e.Schedule.Next(now)
+					c.logger.Info("run", "now", now, "entry", e.ID, "next", e.Next)
+				}
+
+			case newEntry := <-c.add:
+				timer.Stop()
+				now = c.now()
+				newEntry.Next = newEntry.Schedule.Next(now)
+				c.entries = append(c.entries, newEntry)
+				c.logger.Info("added", "now", now, "entry", newEntry.ID, "next", newEntry.Next)
+
+			case <-c.stop:
+				timer.Stop()
+				c.logger.Info("stop")
+				return
+
+			case id := <-c.remove:
+				timer.Stop()
+				now = c.now()
+				c.removeEntry(id)
+				c.logger.Info("removed", "entry", id)
+			}
+
+			break
+		}
+	}
+}
+
+// startJob runs the given job in a new goroutine.
+func (c *Cron) startJob(j Job) {
+	c.jobWaiter.Add(1)
+	ants.Submit(func() {
+		defer c.jobWaiter.Done()
+		j.Run()
+	})
+}
+
+// now returns current time in c location
+func (c *Cron) now() time.Time {
+	return time.Now().In(c.location)
+}
+
+// Stop stops the cron scheduler if it is running; otherwise it does nothing.
+// A context is returned so the caller can wait for running jobs to complete.
+func (c *Cron) Stop() context.Context {
+	c.runningMu.Lock()
+	defer c.runningMu.Unlock()
+	if c.running {
+		c.stop <- struct{}{}
+		c.running = false
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	ants.Submit(func() {
+		c.jobWaiter.Wait()
+		cancel()
+	})
+	return ctx
+}
+
+func (c *Cron) removeEntry(id interface{}) {
+	var entries []*Entry
+	for _, e := range c.entries {
+		if value, ok := id.(EntryID); ok {
+			if value != e.ID {
+				entries = append(entries, e)
+			}
+		} else if value, ok := id.(string); ok {
+			if value != e.Name {
+				entries = append(entries, e)
+			}
+		}
+	}
+	c.entries = entries
 }
